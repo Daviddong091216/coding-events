2eaacb0f403871777bd59043024c05fa442a1a22
David Dong <daviddong201712@gmail.com>
2020-05-25 22:15:48 -0500

    chapter 12.4 is finished. Model binding.

--- src/main/java/com/example/codingevents/controllers/EventController.java
+++ src/main/java/com/example/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import com.example.codingevents.data.EventData;
 import com.example.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +12,7 @@ import java.util.List;
 @Controller
 @RequestMapping("coding-events")
 public class EventController {
-//    private static List<Event> eventsList = new ArrayList<>();
-
+    //coding-events
     @GetMapping
     public String displayAllEvents(Model model) {
         model.addAttribute("events", EventData.getAll());
@@ -33,8 +29,8 @@ public class EventController {
 
     //coding-events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
-        EventData.add(new Event(eventName, eventDescription));
+    public String createEvent(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:";
 
     }
