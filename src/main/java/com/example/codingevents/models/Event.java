e957e413f27c8c31d5f085049bbccee164e748bd
David Dong <daviddong201712@gmail.com>
2020-05-28 11:08:19 -0500

    chapter 18.5.4 is finished. Creating a many-to-many relationship.

--- src/main/java/com/example/codingevents/models/Event.java
+++ src/main/java/com/example/codingevents/models/Event.java
@@ -1,13 +1,12 @@
 package com.example.codingevents.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Event extends AbstractEntity {
@@ -16,14 +15,18 @@ public class Event extends AbstractEntity {
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
 
-    @ManyToOne
-    @NotNull(message = "Category is required.")
-    private EventCategory eventCategory;
-
     @OneToOne(cascade = CascadeType.ALL)
     @Valid
     @NotNull
     private EventDetails eventDetails;
+
+    @ManyToOne
+    @NotNull(message = "Category is required.")
+    private EventCategory eventCategory;
+
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
+
 
     public Event(String name, EventCategory eventCategory) {
         this.name = name;
@@ -41,6 +44,14 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
+    public EventDetails getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
+    }
+
     public EventCategory getEventCategory() {
         return eventCategory;
     }
@@ -49,12 +60,12 @@ public class Event extends AbstractEntity {
         this.eventCategory = eventCategory;
     }
 
-    public EventDetails getEventDetails() {
-        return eventDetails;
+    public List<Tag> getTags() {
+        return tags;
     }
 
-    public void setEventDetails(EventDetails eventDetails) {
-        this.eventDetails = eventDetails;
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
--- src/main/java/com/example/codingevents/models/Tag.java
+++ src/main/java/com/example/codingevents/models/Tag.java
@@ -1,13 +1,19 @@
 package com.example.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Tag extends AbstractEntity {
 
     @Size(min = 1, max = 20, message = "Name must be between 1 and 20 characters.")
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private final List<Event> events = new ArrayList<>();
 
     public Tag(String name) {
         this.name = name;
@@ -22,6 +28,10 @@ public class Tag extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
